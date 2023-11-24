da1f6f9d767f5045e04d9e855d00b6c6e92c66e2
itspatkar <94187044+itspatkar@users.noreply.github.com>
2023-11-24 18:33:34 +0530

    Updated files

--- PHP/update_question.php
+++ PHP/update_question.php
@@ -4,13 +4,12 @@ require "connection.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     $question = $_POST["question"];
-    //$question_id = $_GET["question_id"];
 
     $sql_q = "SELECT question_id, question FROM questionset WHERE question_status = '0'";
     $result_q = $conn->query($sql_q);
 
     if (isset($question_id)) {
-        header("location: update_question_form.php");
+        header("location: ../update_question_form.php");
     } else {
         $count = count($question);
         for ($i = 0; $i < $count; $i++) {
@@ -22,31 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
         }
     }
 
-    // $sql = "DELETE FROM questionset";
-    // if ($conn->query($sql) === TRUE) {
-    //     // Pass
-    // } else {
-    //     echo "Error: " . $sql . "<br>" . $conn->error;
-    // }
-
-    // for ($i = 0; $i < count($question); $i++) {
-    //     $sql = "SELECT question_id FROM questionset";
-    //     $result = $conn->query($sql);
-
-    //     if ($result->num_rows > 0) {
-    //         while ($row = $result->fetch_assoc()) {
-    //         }
-    //     }
-
-
-    //     $sql = "INSERT INTO questionset (question) VALUES ('$question[$i]')";
-    //     if ($conn->query($sql) === TRUE) {
-    //         echo "Questions added successfully.";
-    //         header("Location: ../update_question_form.php");
-    //     } else {
-    //         echo "Error: " . $sql . "<br>" . $conn->error;
-    //     }
-    // }
-
-    $conn->close();
+    header("location: ../update_question_form.php");
 }
+
+$conn->close();
---
+++ crud_db.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2023 at 02:00 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `certificates`
+--
+
+CREATE TABLE `certificates` (
+  `student_id` int(11) NOT NULL,
+  `cert_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `city_id` int(11) NOT NULL,
+  `city_name` text NOT NULL,
+  `state_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`city_id`, `city_name`, `state_id`) VALUES
+(1, 'Mumbai', 14),
+(2, 'Pune', 14);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `student_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `answer` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questionset`
+--
+
+CREATE TABLE `questionset` (
+  `question_id` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `question_status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `student_id` int(11) NOT NULL,
+  `skill_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skillset`
+--
+
+CREATE TABLE `skillset` (
+  `skill_id` int(11) NOT NULL,
+  `skill_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `skillset`
+--
+
+INSERT INTO `skillset` (`skill_id`, `skill_name`) VALUES
+(1, 'Android Development'),
+(2, 'MERN Stack'),
+(3, 'Java Fullstack'),
+(4, 'Python Django'),
+(5, 'Core PHP'),
+(6, 'PHP Laravel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `state_id` int(11) NOT NULL,
+  `state_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`state_id`, `state_name`) VALUES
+(1, 'Andhra Pradesh'),
+(2, 'Arunachal Pradesh'),
+(3, 'Assam'),
+(4, 'Bihar'),
+(5, 'Chhattisgarh'),
+(6, 'Goa'),
+(7, 'Gujarat'),
+(8, 'Haryana'),
+(9, 'Himachal Pradesh'),
+(10, 'Jharkhand'),
+(11, 'Karnataka'),
+(12, 'Kerala'),
+(13, 'Madhya Pradesh'),
+(14, 'Maharashtra'),
+(15, 'Manipur'),
+(16, 'Meghalaya'),
+(17, 'Mizoram'),
+(18, 'Nagaland'),
+(19, 'Odisha'),
+(20, 'Punjab'),
+(21, 'Rajasthan'),
+(22, 'Sikkim'),
+(23, 'Tamil Nadu'),
+(24, 'Telangana'),
+(25, 'Tripura'),
+(26, 'Uttar Pradesh'),
+(27, 'Uttarakhand'),
+(28, 'West Bengal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(100) NOT NULL,
+  `name` text NOT NULL,
+  `dob` date NOT NULL,
+  `gender` text NOT NULL,
+  `state` text NOT NULL,
+  `city` text NOT NULL,
+  `address` text NOT NULL,
+  `photo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `questionset`
+--
+ALTER TABLE `questionset`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `skillset`
+--
+ALTER TABLE `skillset`
+  ADD PRIMARY KEY (`skill_id`);
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`state_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `questionset`
+--
+ALTER TABLE `questionset`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `skillset`
+--
+ALTER TABLE `skillset`
+  MODIFY `skill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `state_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
